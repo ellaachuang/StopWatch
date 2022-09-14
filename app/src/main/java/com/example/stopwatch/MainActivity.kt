e42f98facce762eddc16c9d89e86e6606dbcd1c3
per1 <ellachuang>
2022-09-14 10:43:17 -0700

    reset & start/stop resets to negative

--- app/src/main/java/com/example/stopwatch/MainActivity.kt
+++ app/src/main/java/com/example/stopwatch/MainActivity.kt
@@ -1,10 +1,20 @@
 package com.example.stopwatch
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.SystemClock
 import android.util.Log
+import android.widget.Button
+import android.widget.Chronometer
+import android.widget.TextView
+import kotlin.concurrent.timer
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var startStopButton: Button
+    private lateinit var timerChronometer: Chronometer
+    private lateinit var resetButton: Button
 
     //make a classwide static constant in kotlin
     companion object{
@@ -15,6 +25,34 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        wireWidgets()
+        var isRunning = false
+        var lastPause : Long = 1
+        resetButton.setBackgroundColor(Color.rgb(90, 90, 180))
+        startStopButton.setBackgroundColor(Color.rgb(40, 120, 40))
+
+        startStopButton.setOnClickListener{
+            if (isRunning) {
+                startStopButton.text = "START"
+                startStopButton.setBackgroundColor(Color.rgb(40, 120, 40))
+                isRunning = false
+                lastPause = SystemClock.elapsedRealtime()
+                timerChronometer.stop()
+            } else {
+                startStopButton.text = "STOP"
+                startStopButton.setBackgroundColor(Color.rgb(200, 80, 80))
+                isRunning = true
+                timerChronometer.setBase(SystemClock.elapsedRealtime())
+                timerChronometer.setBase(timerChronometer.getBase() + SystemClock.elapsedRealtime() - lastPause)
+                timerChronometer.start()
+            }
+        }
+
+        resetButton.setOnClickListener {
+            timerChronometer.setBase(SystemClock.elapsedRealtime());
+        }
+
     }
 
     override fun onStart() {
@@ -41,6 +79,14 @@ class MainActivity : AppCompatActivity() {
         super.onDestroy()
         Log.d(TAG, "onDestroy: ")
     }
+
+    private fun wireWidgets() {
+        startStopButton = findViewById(R.id.button_main_start)
+        resetButton = findViewById(R.id.button_main_reset)
+        timerChronometer = findViewById(R.id.chronometer_main_stopwatch)
+    }
+
+
 
 
 
